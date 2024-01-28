54bbfcb8e7d5a433776f665dd3e59e1772cc15c1
subrrahmanyem <subbuthoto91@gmail.com>
2024-01-28 23:39:33 +0530

    update new

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import SearchIcon from "./assets/searchIcon.svg";
 import './App.css';
 import MovieCard from './MovieCard';
+import {ThreeDots} from 'react-loader-spinner';
 
 // Use HTTPS for the API_URL
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=5470d370";
@@ -10,8 +11,10 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [movieData, setMovieData] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const searchMovies = async (title) => {
+    setLoading(true);
     try {
       const response = await fetch(`${API_URL}&s=${title}`);
       
@@ -28,6 +31,8 @@ function App() {
       console.error(`An error occurred: ${error.message}`);
       setMovieData(null);
       setError(error.message); // Set the error message in the state
+    }finally{
+      setLoading(false)
     }
   }
 
@@ -43,6 +48,12 @@ function App() {
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            console.log("key Enter")
+            if (e.key === 'Enter') {
+              searchMovies(searchTerm);
+            }
+          }}
           placeholder="Search for movies"
         />
         <img
@@ -53,7 +64,22 @@ function App() {
       </div>
       {error && <p>Error: {error}</p>}
 
-      {movieData?.length > 0 ? (
+      {loading ? (
+       
+          <div>
+          <ThreeDots
+            visible={true}
+            height="80"
+            width="80"
+            color="#f9d3b4"
+            radius="9"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+          </div>
+      
+        ): (movieData?.length > 0 ? (
         <div className="container">
           {movieData.map((movie) => (
             <MovieCard key={movie.imdbID} movie={movie} />
@@ -63,6 +89,7 @@ function App() {
         <div className="empty">
           <h2>No movies found</h2>
         </div>
+      )
       )}
     </div>
   );
